28b4dba4ea6793239728099780e783378376017d
João Roberto <joao.roberto@ufv.br>
2022-07-08 09:11:04 -0300

    Reset and minus buttons

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ var x = localStorage.getItem('count');
 if (x == null){
 localStorage.setItem('count','0');}
 
-function contador(){
+function plus_cont(){
 	var aux = localStorage.getItem('count');
 	aux = parseInt(aux);
 	aux ++;
 	localStorage.setItem('count',aux);
+}
+
+function minus_cont(){
+	var aux = localStorage.getItem('count');
+	aux = parseInt(aux);
+	aux --;
+	localStorage.setItem('count',aux);
+}
+
+function z_cont(){
+	localStorage.setItem('count','0');
 }
